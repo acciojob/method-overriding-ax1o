4b25567eba1d8bf51d54deb064496abb33b63e50
Siddhant <siddhantgk01@gmail.com>
2023-02-09 23:17:42 +0530

    First Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,29 @@
 package com.driver;
 
 public class Main {
+
+    class A{
+
+        public String meth(){
+            return "S";
+        }
+
+    }
+
+    class B extends  A{
+
+        @Override
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+
+    }
+
+    public void main(String[] args){
+
+        B b = new B();
+        b.meth();
+
+    }
   
 }
